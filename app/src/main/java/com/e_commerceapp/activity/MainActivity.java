01ae1ef1bc33c4c447106368860fca98d22b3be1
Laura Lamprea <laura.lamprea@uniminuto.edu.co>
2022-11-20 20:09:56 -0500

    Creacion de login y perfil

--- app/src/main/java/com/e_commerceapp/activity/MainActivity.java
+++ app/src/main/java/com/e_commerceapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.e_commerceapp;
+package com.e_commerceapp.activity;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.e_commerceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
